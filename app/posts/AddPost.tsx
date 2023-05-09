3b522dcef5f11f4a1941c290ce3fdc5cae426b48
jackconwayiv <jackconwayiv@gmail.com>
2023-05-09 15:21:35 -0700

    Chakra working in components

---
+++ app/posts/AddPost.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button, Heading } from "@chakra-ui/react";
+
+const AddPost = () => {
+  return (
+    <>
+      <Heading>Add Post</Heading>
+      <Button>Button</Button>
+    </>
+  );
+};
+
+export default AddPost;
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,10 @@
+import AddPost from "./AddPost";
+
 export default async function PostsPage() {
   return (
     <div>
       <h1>POSTS</h1>
+      <AddPost />
     </div>
   );
 }
